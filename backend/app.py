4602c8f733dd20caad8abbdf78a7130662e86e9b
Muhammad Zahran <zahranmuhammad267@gmail.com>
2025-06-30 20:14:21 +0700

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -40,5 +40,4 @@ def home():
     return send_from_directory('../frontend', 'index.html')
 
 if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 10000))
-    app.run(host='0.0.0.0', port=port, debug=True)
+    app.run(host='0.0.0.0', port=3000)
